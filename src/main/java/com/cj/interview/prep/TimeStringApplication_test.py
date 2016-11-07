1de89492461cf162815719a20b482262fa7f9118
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-07 14:18:19 -0800

    Modify test to accept required input

--- src/main/java/com/cj/interview/prep/TimeStringApplication_test.py
+++ src/main/java/com/cj/interview/prep/TimeStringApplication_test.py
@@ -2,6 +2,6 @@ import nose.tools
 from TimeStringApplication import TimeStringApplication
 
 def test_flux_capacitor():
-    say = "1.21 Gigawatts!"
+    say = "12:32 34:01 15:23 9:27 55:22 25:56" 
     mr_fusion = TimeStringApplication()
     assert mr_fusion.flux_capacitor(say) == say
