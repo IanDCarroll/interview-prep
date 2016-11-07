d59d4533ab9fc494cd2acc2cc1dc3fa0261b49c1
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-07 14:20:34 -0800

    refactor test to fail as is propper

--- src/main/java/com/cj/interview/prep/TimeStringApplication_test.py
+++ src/main/java/com/cj/interview/prep/TimeStringApplication_test.py
@@ -2,6 +2,7 @@ import nose.tools
 from TimeStringApplication import TimeStringApplication
 
 def test_flux_capacitor():
-    say = "12:32 34:01 15:23 9:27 55:22 25:56" 
+    question = "12:32 34:01 15:23 9:27 55:22 25:56"
+    answer = "2:32:41"
     mr_fusion = TimeStringApplication()
-    assert mr_fusion.flux_capacitor(say) == say
+    assert mr_fusion.flux_capacitor(question) == answer
